7f58246ca7d5f6f69a7c1a3f5b10f4835670a324
Jakub Arnold <darthdeus@gmail.com>
2015-06-06 19:44:40 +0200

    Attempt at improving the monadic parser, wip

--- src/Scheme/Parser.hs
+++ src/Scheme/Parser.hs
@@ -1,164 +1,77 @@
 module Scheme.Parser where
 
+import Control.Applicative
 import Control.Monad
 import qualified Data.Char as C
 
 data Parser a = Parser (String -> [(a, String)])
 
-item :: Parser Char
-item = Parser (\s -> case s of
+runParser :: Parser a -> (String -> [(a, String)])
+runParser (Parser p) = p
+
+anyChar :: Parser Char
+anyChar = Parser (\s -> case s of
                      "" -> []
-                     (c:cs) -> [(c,cs)])
+                     (x:xs) -> [(x, xs)])
 
-parse :: Parser a -> (String -> [(a, String)])
-parse (Parser x) = x
+failed :: Parser a
+failed = Parser (\_ -> [])
 
-unit :: a -> Parser a
-unit a = Parser (\s -> [(a,s)])
+instance Functor Parser where
+  fmap f (Parser p) = Parser (\x -> do
+                                 (a, s) <- p x
+                                 Just (f a, s))
 
--- Obracena varianta $ pro lepsi prehlednost
-(|>) :: b -> (b -> c) -> c
-(|>) = flip ($)
+instance Applicative Parser where
+  pure x = Parser (\s -> Just (x, s))
+  (Parser f) <*> (Parser g) = Parser (\x -> do
+                                         (a, s) <- f x
+                                         (b, t) <- g s
 
-bind :: Parser a -> (a -> Parser b) -> Parser b
-bind x f = Parser g
-           where g = (\s -> concatMap m $ parse x s)
-                 m = \(a, s') -> parse (f a) s'
-
-satisfies :: (Char -> Bool) -> Parser Char
-satisfies f = item `bind` \c ->
-              if f c
-              then unit c
-              else (Parser (\_ -> []))
+                                         return $ (a b, t))
 
 instance Monad Parser where
-   return a = Parser (\s -> [(a,s)])
-   p >>= f = Parser (\s -> parse p s |>
-                         map (\ (a, s') -> parse (f a) s') |>
-                         concat)
+  return = pure
+  (Parser p) >>= f = Parser (\x -> do
+                                (a, s) <- p x
+                                let (Parser q) = f a
+                                q s)
 
-instance MonadPlus Parser where
-         mzero = Parser (\cs -> [])
-         mplus p q = Parser (\s -> (parse p) s ++ (parse q) s)
+run :: String -> Parser a -> Maybe a
+run s (Parser f) = case f s of
+  Just (x, _) -> Just x
+  Nothing -> Nothing
 
-option :: Parser a -> Parser a -> Parser a
-option x y = Parser (\s -> case parse (mplus x y) s of
-                                [] -> []
-                                (z:_) -> [z])
+satisfies :: (Char -> Bool) -> Parser Char
+satisfies p = do
+  c <- anyChar
+
+  if p c
+    then return c
+    else failed
 
 char :: Char -> Parser Char
 char c = satisfies (c ==)
 
 string :: String -> Parser String
 string "" = return ""
-string (c:cs) = char c >> string cs >> return (c:cs)
+string (x:xs) = do
+  y <- char x
+  ys <- string xs
+  return (y:ys)
+
+(<|>) :: Parser a -> Parser a -> Parser a
+(<|>) p q = Parser (\x ->
+                      case runParser p x of
+                        Just res -> return res
+                        Nothing -> runParser q x)
+infixr 1 <|>
 
 many :: Parser a -> Parser [a]
-many p = many1 p `option` return []
+many p = many1 p <|> return []
 
 many1 :: Parser a -> Parser [a]
-many1 p = do { a <- p; as <- many p; return (a:as)}
-
-sepBy :: Parser a -> Parser b -> Parser [a]
-p `sepBy` sep = (p `sepBy1` sep) `option` return []
-
-sepBy1 :: Parser a -> Parser b -> Parser [a]
-p `sepBy1` sep = do a <- p
-                    as <- many $ sep >> p
-                    return (a:as)
-
-space :: Parser String
-space = many (satisfies isSpace)
-       where isSpace ' '  = True
-             isSpace '\n' = True
-             isSpace '\t' = True
-             isSpace _    = False
-
-token :: Parser a -> Parser a
-token p = do
-  a <- p
-  space
-  return a
-
-symb :: String -> Parser String
-symb s = token (string s)
-
-digit :: Parser Char
-digit = satisfies C.isDigit
-
-number :: Parser Int
-number = do cs <- many1 digit
-            return $ read cs
-
-run :: String -> Int
-run s = case parse expr s of
-             [(num, _)] -> num
-             _ -> -1
-
-chainl :: Parser a -> Parser (a -> a -> a) -> a -> Parser a
-chainl x op a = (x `chainl1` op) `option` return a
-
-chainl1 :: Parser a -> Parser (a -> a -> a) -> Parser a
-chainl1 x op = do {a <- x; rest a}
-                 where rest a = (do f <- op
-                                    b <- x
-                                    rest (f a b))
-                                `option` return a
-
-
--- p :: Parser (Char,Char)
--- p = do
---   c <- item
---   item
---   d <- item
---   return (c, d)
-
-
-expr :: Parser Int
-expr = term `chainl1` addop
-
-term = factor `chainl1` mulop
-factor = number `option` do { symb "("; n <- expr; symb ")"; return n}
-
-addop :: Parser (Int -> Int -> Int)
-addop = (symb "+" >> return (+)) `option` (symb "-" >> return (-))
-
-mulop :: Parser (Int -> Int -> Int)
-mulop = (symb "*" >> return (*)) `option` (symb "/" >> return (div))
-
-data JSONVal = JBool Bool |
-               JString String |
-               JNumber Int |
-               JNull |
-               JArray [JSONVal] |
-               JPair (String, JSONVal) |
-               JObject [(String, JSONVal)]
-               deriving (Show)
-
-
--- parseBool :: Parser JSONVal
--- parseBool = do { symb "true"; return $ JBool True} `option`
---             do { symb "false"; return $ JBool False }
-
--- parseString :: Parser JSONVal
--- parseString = do { s <- string; return $ JString s}
-
--- parseNumber :: Parser JSONVal
--- parseNumber = do { n <- number; return $ JNumber n}
-
--- parseNull :: Parser JSONVal
--- parseNull = do { symb "null"; return JNull}
-
--- parseArray :: Parser JSONVal
--- parseArray = do { symb "["; l <- parseJson `sepBy` (symb ","); symb "]"; return $ JArray l}
-
--- parsePair :: Parser JSONVal
--- parsePair = do { k <- string; symb ":"; v <- parseJson; return $ JPair (k,v)}
-
--- parseObject :: Parser JSONVal
--- parseObject = do { symb "{"; ps <- many parsePair ; symb "}";
---                    return $ JObject $ map (\ (JPair p) -> p) ps}
-
--- parseJson :: Parser JSONVal
--- parseJson = parseBool `option` parseString `option` parseNumber `option`
---             parseNull `option` parseArray `option` parseObject
+many1 p = do
+  x <- p
+  xs <- many p
+  return (x:xs)
